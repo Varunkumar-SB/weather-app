bada0e3dd0341f6432eb29990c2771c8170b1f52
Varunkumar <sbvarunkumar05@gmail.com>
2023-12-30 22:52:02 +0530

    bug fix

--- js/index.js
+++ js/index.js
@@ -259,13 +259,13 @@ function getWeatherInfo() {
       .then((data) => {
         weatherInfo = data;
         displayWeather(weatherInfo);
+        document.getElementById("weatherData").style.display = "block";
+        document.getElementById("locationForm").style.display = "none";
       })
       .catch((error) => {
         console.error("API Error:", error);
       });
   }
-  document.getElementById("weatherData").style.display = "none";
-  document.getElementById("locationForm").style.display = "flex";
 }
 
 document
