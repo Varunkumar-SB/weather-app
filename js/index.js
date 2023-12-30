66dc22a53ec753256d89fb7b6283a9408f7c5959
Varunkumar <sbvarunkumar05@gmail.com>
2023-12-30 22:03:21 +0530

    security improvements

--- js/index.js
+++ js/index.js
@@ -35,8 +35,6 @@ if (localStorage.getItem("localStorageUnit")) {
   document.getElementById(localStorageUnit).checked = true;
 }
 
-getWeatherInfo();
-
 const theme = localStorage.getItem("theme");
 if (theme) {
   if (theme === "dark") {
@@ -231,10 +229,6 @@ function getWeatherInfo() {
   const isAnyRadioSelected = [...metricRadios].some((radio) => radio.checked);
 
   if (!isAnyRadioSelected || !countryInput.value || !cityInput.value) {
-    document.getElementById("toggleLocationForm").checked = true;
-    setTimeout(() => {
-      document.getElementById("locationForm").style.display = "flex";
-    }, 90);
     return;
   } else {
     localStorage.setItem("localStorageCity", cityInput.value);
@@ -265,10 +259,6 @@ function getWeatherInfo() {
       .then((data) => {
         weatherInfo = data;
         displayWeather(weatherInfo);
-        document.getElementById("toggleLocationForm").checked = false;
-        setTimeout(() => {
-          document.getElementById("locationForm").style.display = "none";
-        }, 90);
       })
       .catch((error) => {
         console.error("API Error:", error);
@@ -312,22 +302,6 @@ function displayWeather(weatherInfo) {
   document.querySelector(".outputWindSpeed").innerText = windSpeed;
   document.querySelector(".outputLastUpdated").innerText = lastUpdated;
 }
-
-function checkChecked() {
-  if (document.getElementById("toggleLocationForm").checked === true) {
-    setTimeout(() => {
-      document.getElementById("locationForm").style.display = "none";
-    }, 90);
-  } else {
-    setTimeout(() => {
-      document.getElementById("locationForm").style.display = "flex";
-    }, 90);
-  }
-}
-
-document
-  .querySelector(".toggleLocationForm")
-  .addEventListener("click", checkChecked);
 
 //#000 End
 
@@ -416,8 +390,32 @@ document.getElementById("changeTheme").addEventListener("click", changeTheme);
 
 //#000 End
 
-window.onload = function () {
-  window.scrollTo({
-    top: 0,
-  });
-};
+//#fff Code to toggle location form
+
+if (
+  localStorage.getItem("localStorageCountry") &&
+  localStorage.getItem("localStorageCity") &&
+  localStorage.getItem("localStorageUnit")
+) {
+  getWeatherInfo();
+  document.getElementById("weatherData").style.display = "block";
+  document.getElementById("locationForm").style.display = "none";
+} else {
+  document.getElementById("weatherData").style.display = "none";
+  document.getElementById("locationForm").style.display = "flex";
+}
+
+document.querySelector(".toggleLocationForm").addEventListener("click", () => {
+  if (
+    window.getComputedStyle(document.getElementById("weatherData")).display ===
+    "block"
+  ) {
+    document.getElementById("weatherData").style.display = "none";
+    document.getElementById("locationForm").style.display = "flex";
+  } else {
+    document.getElementById("weatherData").style.display = "block";
+    document.getElementById("locationForm").style.display = "none";
+  }
+});
+
+//#000 End
